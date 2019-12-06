22a95e57b7cb28cf1b3ccb6683a8f9e1ce67b38f
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 21:58:27 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,6 @@
 class SecretsController < ApplicationController
   before_action :require_login
-
+  # The content and URL of the secret page are up to you. However, if we visit that URL without logging in, we should be redirected to the login page. Under no circumstances should we allow people who are not logged in to see the secrets.
   def show
     @user = current_user
   end
