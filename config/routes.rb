1e431c82935854ec38fc2275bb59a9594ba4eb61
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 20:33:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-
+  # Visit the root of the app and be redirected to a login page.
   root 'sessions#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
