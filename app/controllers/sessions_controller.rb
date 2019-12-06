cf7e8bf73290247ff62f59c89f568aaef4dfb2df
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 20:34:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
   # If a user fails to enter their name on the login page, they should be redirected there until they successfully do so.
   def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
   end
 
   def destroy
