65de88163f8f849cf2c9df688d9dbb6bbb6a78aa
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 21:06:10 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,12 @@
 class SecretsController < ApplicationController
-  before_action :current_user
+  before_action :require_login
 
   def show
     redirect_to secrets_show_path
+  end
+
+  private
+
+  def require_login
   end
 end
