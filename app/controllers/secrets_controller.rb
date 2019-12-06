d5c7f1f22db4d70506d948c32cb1609a48b6bf90
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 21:03:15 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,7 @@
 class SecretsController < ApplicationController
   def show
     if current_user
-      redirect_to secret_path
+      redirect_to secrets_show_path
     else
       redirect_to sessions_new_path
     end
