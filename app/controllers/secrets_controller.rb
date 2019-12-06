94b085c91d38a5962387d12be6b6cf17973ac2d2
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 21:07:42 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -8,6 +8,6 @@ class SecretsController < ApplicationController
   private
 
   def require_login
-    return head(:forbidden) unless current_user
+    redirect_to sessions_new_path unless current_user
   end
 end
