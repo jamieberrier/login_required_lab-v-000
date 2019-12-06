40c9574d431340ac12343cbe902cb91f14a9e23c
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 21:08:54 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -8,6 +8,6 @@ class SecretsController < ApplicationController
   private
 
   def require_login
-    redirect_to sessions_new_path unless current_user
+    redirect_to sessions_new_path unless !current_user.nil?
   end
 end
