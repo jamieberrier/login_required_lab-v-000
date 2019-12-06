f7acc51cfaf3f6060e91ea4f72533f9cd6c0ea6b
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 20:33:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
-  root 'sessions#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+
+    root 'sessions#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
