5d7de8e96a36b844088ab830ab589d016ee9602a
Jamie Berrier <berrier.jamie@gmail.com>
2019-12-06 20:29:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
+  root 'application#home'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
